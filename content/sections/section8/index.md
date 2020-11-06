3bd114e92b14229ff99e3e1e154818fd5c98a846
mariosmat <mariosmat@g.harvard.edu>
2020-11-06 11:56:50 -0500

    section8

---
+++ content/sections/section8/index.md
@@ -0,0 +1,10 @@
+Title: S-Section 08: Review Trees and Boosting including Ada Boosting Gradient Boosting and XGBoost
+Category: sections
+Slug: section8
+Author: Marios Mattheakis, Hayden Joy
+Date: 2020-11-06
+Tags: decision trees, bagging, random forest, boosting, adaboost and xgboost
+
+## Jupyter Notebooks
+
+- [S-Section 8: Review Trees and Boosting including Ada Boosting Gradient Boosting and XGBoost]({filename}notebook/cs109a_section_8.ipynb)
---
+++ docs/sections/section8/exercises/bo1.py
@@ -0,0 +1,19 @@
+def get_impurity_pd(model, n = 0):
+    """
+    This function returns a pandas dataframe with all of the nth nodes feature impurities.
+    """
+    rf_estimators = model.estimators_.copy()
+    features = np.array(X_train.columns)
+    
+    node_impurities, node_features = [], []
+
+    for i, estimator in enumerate(rf_estimators):
+        estimator_impurity = #TODO 0 
+        estimator_feature  = #TODO 1
+        
+        node_impurities.append(estimator_impurity)
+        node_features.append(estimator_feature)
+    node_impurity_dict = {"feature": #TODO 
+                        "impurity": #TODO
+    df = #TODO
+    return(df)
